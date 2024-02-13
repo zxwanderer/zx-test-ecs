714e3e3e994e4c3e4d1eb39251c8a0e33292540c
zxwanderer <no@mail.com>
2024-02-13 15:20:44 +0300

    updates

--- TABLE_SCAN_BY_INDEX_PTR.asm
+++ TABLE_SCAN_BY_INDEX_PTR.asm
@@ -1,14 +1,6 @@
   MODULE TestSuite_TABLE_SCAN_BY_INDEX_PTR
 
-  include "lib/zx-core/libs/map/defines_h.asm"
-  ; include "lib/zx-core/libs/macros/macros_h.asm"
-  ; include "lib/zx-core/libs/tables/scan_by_index.asm"
-
-check_act_yes:
-  retTrue
-
-check_act_no:
-  retFalse
+  include "lib/zx-core/libs/tables/scan_by_index.asm"
 
 UT_clear_screen:
 
