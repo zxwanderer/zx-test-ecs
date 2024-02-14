ac495310e979c0e7b3d7ff7d228e6e75501bc968
zxwanderer <no@mail.com>
2024-02-14 12:44:58 +0300

    updates

--- ecs.asm
+++ ecs.asm
@@ -1,13 +1,27 @@
 
-@__component_number__ = 0
+@__component_number__ = 1
 
 ; как RuGuevara завещал
 
   MACRO DefineAttribute name?
 name? equ @__component_number__
 @__component_number__ = @__component_number__ + 1
-  ASSERT @__component_number__ < 3
+  ASSERT @__component_number__ < 100
   ENDM
 
+; аттрибуты - однобайтовые параметры
+  DefineAttribute Breakable ; разбивается
+  DefineAttribute Standable ; можно встать на предмет
   DefineAttribute Tools
   DefineAttribute Door
+
+; Entity:
+; ------
+; [parent_ptr] - родительский предмет (?) или #ffff
+; [info_text_ptr] - текстовое описание предмета или #ffff
+; ------
+; [attribute] 
+; ...
+; [attribute]
+; [_endByte]
+; ------
